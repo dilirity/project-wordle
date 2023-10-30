1b28738eaea6fa7b06a95e860a2ed31b767fbedb
Peter Petrov <peter.petrov89@gmail.com>
2023-10-30 16:21:37 +0200

    Update UI to include guess feedback

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers'
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const guessStatus = checkGuess( value, answer );
+
   return (
     <p className="guess">
       {range(5).map((num) => (
         <span
           key={num}
-          className="cell"
+          className={`cell ${value ? guessStatus[num].status : ''}`}
         >{value ? value[num] : undefined}</span>
       ))}
     </p>
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -5,13 +5,14 @@ import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 import Guess from '../Guess/Guess';
 
-function GuessList({guesses}) {
+function GuessList({guesses, answer}) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
         <Guess
           key={num}
           value={guesses[num]}
+          answer={answer}
         />
       ))}
     </div>
